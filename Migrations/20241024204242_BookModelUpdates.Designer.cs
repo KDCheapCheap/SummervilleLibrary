35308ed4ad31b63a64d99ec8f9de71d5155c3f0a
Kyle Davison <kyle_davison@outlook.com>
2024-10-24 21:43:10 +0100

    Added BookModelUpdates migration

--- Migrations/20241024204242_BookModelUpdates.Designer.cs
+++ Migrations/20241024204242_BookModelUpdates.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SummervilleLibrary.Data;
@@ -11,9 +12,11 @@ using SummervilleLibrary.Data;
 namespace SummervilleLibrary.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241024204242_BookModelUpdates")]
+    partial class BookModelUpdates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20241024204242_BookModelUpdates.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SummervilleLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class BookModelUpdates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Blurb",
+                table: "Books");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PublicationYear",
+                table: "Books",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublicationYear",
+                table: "Books");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Blurb",
+                table: "Books",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
